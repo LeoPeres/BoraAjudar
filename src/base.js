1f7cc301c27e65f98e958a1442d1d440f7381a14
Leonardo <leo_ps.12@hotmail.com>
2018-05-07 16:31:20 -0300

    Iniciando

---
+++ src/base.js
@@ -0,0 +1,15 @@
+import Rebase from 're-base'
+import firebase from 'firebase'
+
+const config = {
+    apiKey: 'AIzaSyBdCUTsdjyXVUxJNPF2Yh63YhsKDUzzoPY',
+    authDomain: 'bora-fe98f.firebaseapp.com',
+    databaseURL: 'https://bora-fe98f.firebaseio.com',
+    projectId: 'bora-fe98f',
+    storageBucket: 'bora-fe98f.appspot.com',
+    messagingSenderId: '463596977499'
+}
+
+const app = firebase.initializeApp(config)
+const base = Rebase.createClass(app.database())
+export default base
